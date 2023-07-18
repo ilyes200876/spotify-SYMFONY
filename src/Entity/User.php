9646cde0abe0a36ba0e896fef7cc09713bc7d826
ilyes200876 <ilyes200876@live.fr>
2023-07-18 16:48:07 +0200

    show array musics/artists and include header/footer

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -125,4 +125,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->userName;
+    }
 }
